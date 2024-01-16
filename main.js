65aa66c237803da7568b75eace06a5abe2dcbeb7
Allen Stevens <allen.stevens@students.dominican.edu>
2024-01-16 10:55:17 -0800

    added paddle and keyboard controls

--- main.js
+++ main.js
@@ -8,6 +8,32 @@ let dy = -2;
 
 const ballRadius = 10;
 
+const paddleHeight = 10;
+const paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+
+let rightPressed = false;
+let leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if (e.key === "Right" || e.key === "ArrowRight") {
+      rightPressed = true;
+    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+      leftPressed = true;
+    }
+  }
+  
+  function keyUpHandler(e) {
+    if (e.key === "Right" || e.key === "ArrowRight") {
+      rightPressed = false;
+    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+      leftPressed = false;
+    }
+  }
+
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y,ballRadius, 0, Math.PI*2);
@@ -15,22 +41,47 @@ function drawBall() {
     ctx.fill();
     ctx.closePath();
 }
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+  }
 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);// clears the canvas
+  function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
-    x += dx; // adds 2 to x every time the draw function is called
-    y += dy;// adds 2 to y every time the draw function is called
-
-    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    drawPaddle();
+    
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
-      }
-      if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
         dy = -dy;
-      }
+    }
+    
+    if(rightPressed) {
+        paddleX += 7;
+        if (paddleX + paddleWidth > canvas.width){
+            paddleX = canvas.width - paddleWidth;
+        }
+    }
+    else if(leftPressed) {
+        paddleX -= 7;
+        if (paddleX < 0){
+            paddleX = 0;
+        }
+    }
+    
+    x += dx;
+    y += dy;
 }
 
 setInterval(draw, 10);
+
+
+
 
 // // draws a rectangle
 // ctx.beginPath(); // tells the robot to start drawing
